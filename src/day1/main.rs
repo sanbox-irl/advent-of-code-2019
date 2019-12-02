981e3df4b00766b7e0ad0a878198e914150f7cb8
Jonathan Spira <jjspira@Jonathans-Mac-mini.local>
2019-12-02 01:21:20 -0500

    cleaned it up

--- src/day1/main.rs
+++ src/day1/main.rs
@@ -6,30 +6,35 @@ fn main() -> Result<(), Box<dyn Error>> {
         .map(|s| s.parse().unwrap())
         .collect();
 
-    let mut fuel = 0;
-    for mass in input {
-        let module_req = convert_mass_to_fuel(mass);
-        fuel += module_req + fuel_for_fuel(module_req);
-    }
+    // part 1
+    println!(
+        "(part1) Fuel for Module is {}",
+        input.iter().map(convert_mass_to_fuel).sum::<usize>()
+    );
 
-    println!("Final is {}", fuel);
-
+    println!(
+        "(part2) Fuel for Module and Fuel is {}",
+        input
+            .iter()
+            .map(|m| fuel_for_fuel(convert_mass_to_fuel(&m)))
+            .sum::<usize>()
+    );
     Ok(())
 }
 
-fn convert_mass_to_fuel(mass: usize) -> usize {
-    ((mass as f32) / 3.0) as usize - 2
+fn convert_mass_to_fuel(mass: &usize) -> usize {
+    ((*mass as f32) / 3.0) as usize - 2
 }
 
 fn fuel_for_fuel(fuel_mass: usize) -> usize {
-    // 9 is the most we give a shit about
     let mut additional_fuel = 0;
     let mut this_iteration_fuel = fuel_mass;
 
+    // 8 is a heuristic, as we can check that we'll never get below or at 0 until 9
     while this_iteration_fuel > 8 {
-        this_iteration_fuel = convert_mass_to_fuel(this_iteration_fuel);
+        this_iteration_fuel = convert_mass_to_fuel(&this_iteration_fuel);
         additional_fuel += this_iteration_fuel;
     }
 
-    additional_fuel
+    additional_fuel + fuel_mass
 }
