3f2d5258d046617dbc85990ba1e9570d10dbc939
Jonathan Spira <jjspira@gmail.com>
2019-12-12 20:17:24 -0500

    starting up day8

---
+++ src/day8/main.rs
@@ -0,0 +1,15 @@
+const X: usize = 25;
+const Y: usize = 6;
+
+fn main() {
+    let input: Vec<usize> = include_str!("input.txt")
+        .chars()
+        .map(|character| character.to_digit(10).unwrap() as usize)
+        .collect();
+
+    let layers: Vec<Vec<usize>> = {
+        for digit in input {
+            if i == X * 
+        }
+    };
+}
